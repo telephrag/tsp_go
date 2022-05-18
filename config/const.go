91192911704071d0bfea22be4de0f64618e0cdbc
trofchik <trofimov.nikita20@gmail.com>
2022-05-18 18:10:55 +0400

    Got rid of most synchronization overhead. Now it's faster than sequential version.

--- config/const.go
+++ config/const.go
@@ -1,4 +1,4 @@
 package config
 
-const RouteQueueSize = 2048 // 16777216
-const MaxTravelers = 1
+const RouteQueueSize = 4096 // 16777216
+const MaxTravelers = 7
--- tsp/solve.go
+++ tsp/solve.go
@@ -1,9 +1,7 @@
 package tsp
 
 import (
-	"context"
 	"sync"
-	"tsp/config"
 )
 
 func nextPermutation(route []int) {
@@ -24,43 +22,42 @@ func getPermutation(original, route []int) []int {
 	return result
 }
 
-func (t *Tsp) calcDistance(route []int) {
+func (t *Tsp) calcDistance(route []int, startNode int) {
 
-	distance := graph[0][route[0]] // add distance from zero-node to the first node in route
-	for i := 0; i < len(route)-1; i++ {
+	distance := graph[0][startNode]        // add distance from zero-node to the first node in route
+	distance += graph[startNode][route[0]] // account for startNode
+	for i := 0; i < len(route)-1; i++ {    // calculate weight of the rest of the permutation
 		distance += graph[route[i]][route[i+1]]
-		// if distance > t.MinDist {
-		// 	return
-		// }
+		if distance > t.MinDist {
+			return
+		}
 	}
 	distance += graph[route[len(route)-1]][0] // return to zero-node
 
+	t.Mu.Lock()
 	if distance < t.MinDist {
-		t.Mu.Lock()
-		// fmt.Printf("Swap: %v <-> %v\n", t.MinRoute, route)
 		t.MinDist = distance
-		copy(t.MinRoute, route)
-		copy(t.MinRoute, t.MinRoute[:1])
-		// fmt.Printf("Swap: %v <-> %v\n", t.MinRoute, route)
-		t.Mu.Unlock()
+		copy(t.MinRoute[2:], route) // copy route into t.MinRoute with shift by two
+		t.MinRoute[1] = startNode   // the zero-elem will be 0 and the first will be startNode
 	}
-
+	t.Mu.Unlock()
 }
 
-func (t *Tsp) travel(routeChan chan []int, ctx context.Context) {
-	for {
-		select {
-		case routeToCalc := <-routeChan: // Possible datarace hence decreased performance
-			t.calcDistance(routeToCalc)
-			continue
-		default:
+func (t *Tsp) travel(startNode int) {
+	nodeSet := make([]int, t.NodeCount-2)
+	j := 0
+	for i := 1; i < t.NodeCount; i++ {
+		if i != startNode {
+			nodeSet[j] = i
+			j++
 		}
+	}
 
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
+	for r := make([]int, len(nodeSet)); r[0] < len(r); nextPermutation(r) {
+		t.calcDistance(
+			getPermutation(nodeSet, r),
+			startNode,
+		)
 	}
 }
 
@@ -69,31 +66,18 @@ func (t *Tsp) Solve() {
 		panic("graph is empty")
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	routeChan := make(chan []int, config.RouteQueueSize) // limit amount of permutations at the same time
-
-	nodeSet := make([]int, t.NodeCount)
-	for i := 1; i < t.NodeCount; i++ {
-		nodeSet[i] = i
+	nodeSet := make([]int, t.NodeCount-1)
+	for i := 0; i < len(nodeSet); i++ {
+		nodeSet[i] = i + 1
 	}
 
-	// consider turning into buffer
-	// consider it to evenly spread routes between travellers
-	go func() {
-		for r := make([]int, len(nodeSet)); r[0] < len(r); nextPermutation(r) {
-			routeChan <- getPermutation(nodeSet, r)
-		}
-		// create permutation in a seperate goroutine
-		cancel() // cancel the context once done
-	}()
-
 	var wg sync.WaitGroup
-	wg.Add(config.MaxTravelers)
-	for i := 0; i < config.MaxTravelers; i++ {
-		go func() {
-			t.travel(routeChan, ctx)
+	wg.Add(len(nodeSet))
+	for _, startNode := range nodeSet {
+		go func(sn int) {
+			t.travel(sn)
 			wg.Done()
-		}()
+		}(startNode)
 	}
 	wg.Wait()
 }
